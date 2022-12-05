964bc167e65c37e8f4bc6bc323d8f1f81a91de0e
erivera <erivera@isertec.com>
2022-12-05 11:26:49 -0600

    Cambios Marcaje Guatemala

--- purchaseTracking/Connection/connectionHana.cs
+++ purchaseTracking/Connection/connectionHana.cs
@@ -13,7 +13,7 @@ namespace purchaseTracking.Connection
         {
             //Connexion con SAP HANA - Se utiliza un usuario de solo lectura en la base de datos debido al acuerdo de licencia
             HanaConnection conn = new HanaConnection();
-            conn.ConnectionString = "Server=192.168.1.221:30015;UID=WEBMASTER;PWD=BTS!st3ms!; Current Schema=SBO_ISERTEC_HN";
+            conn.ConnectionString = "Server=192.168.1.221:30015;UID=WEBMASTER;PWD=BTS!st3ms!; Current Schema=SBO_ISERTEC_GT";
             conn.Open();
             return conn;
         }
--- purchaseTracking/Connection/eTalentConnection.cs
+++ purchaseTracking/Connection/eTalentConnection.cs
@@ -12,7 +12,7 @@ namespace purchaseTracking.Connection
         {
             string connetionString;
             SqlConnection cnn;
-            connetionString = @"Data Source=10.93.110.173;Initial Catalog=db_eaccess_isertec_hn;User ID=sa;Password=manag3RS";
+            connetionString = @"Data Source=192.168.1.185;Initial Catalog=db_nomina_isertec_v5_prod;User ID=sa;Password=manag3RS";
             cnn = new SqlConnection(connetionString);
             cnn.Open();
             return cnn;
